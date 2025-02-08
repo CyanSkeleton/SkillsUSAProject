2bf9ff4063725d9359e920c5d883fc9e7d2086cc
CyanSkeleton <jhaanubhav8305@gmail.com>
2025-02-08 13:03:30 -0800

    final commit

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ def is_palindrome(string, case_insensitive=False, whitespace_insensitive=False, 
             # The above line raises type error, but it's an "an" instead of an "a"
 
     if not isinstance(case_insensitive, bool):  # Check if case_insensitive is a boolean
-        if not isinstance(string, int) or isinstance(string, bool):
+        if not isinstance(case_insensitive, int) or isinstance(case_insensitive, bool):
             # The above line checks if the input is not an int or is a bool
             # to make sure correct grammar is used in the error message
             raise TypeError(f"case_insensitive must be a Boolean, not a {type(case_insensitive).__name__}")
@@ -24,22 +24,22 @@ def is_palindrome(string, case_insensitive=False, whitespace_insensitive=False, 
             raise TypeError(f"case_insensitive must be a Boolean, not an {type(case_insensitive).__name__}")
             # The above line raises type error, but it's an "an" instead of an "a"
     if not isinstance(whitespace_insensitive, bool):  # Check if whitespace_insensitive is a boolean
-        if not isinstance(string, int) or isinstance(string, bool):
+        if not isinstance(whitespace_insensitive, int) or isinstance(whitespace_insensitive, bool):
             # The above line checks if the input is not an int or is a bool (a subclass of int)
             # to make sure correct grammar is used in the error message
-            raise TypeError(f"case_insensitive must be a Boolean, not a {type(whitespace_insensitive).__name__}")
+            raise TypeError(f"whitespace_insensitive must be a Boolean, not a {type(whitespace_insensitive).__name__}")
             # The above line raises a type error
         else:
-            raise TypeError(f"case_insensitive must be a Boolean, not an {type(whitespace_insensitive).__name__}")
+            raise TypeError(f"whitespace_insensitive must be a Boolean, not an {type(whitespace_insensitive).__name__}")
             # The above line raises type error, but it's an "an" instead of an "a"
     if not isinstance(ignore_punctuation, bool):  # Check if ignore_punctuation is a boolean
-        if not isinstance(string, int) or isinstance(string, bool):
+        if not isinstance(ignore_punctuation, int) or isinstance(ignore_punctuation, bool):
             # The above line checks if the input is not an int or is a bool
             # to make sure correct grammar is used in the error message
-            raise TypeError(f"case_insensitive must be a Boolean, not a {type(ignore_punctuation).__name__}")
+            raise TypeError(f"ignore_punctuation must be a Boolean, not a {type(ignore_punctuation).__name__}")
             # The above line raises a type error
         else:
-            raise TypeError(f"case_insensitive must be a Boolean, not an {type(ignore_punctuation).__name__}")
+            raise TypeError(f"ignore_punctuation must be a Boolean, not an {type(ignore_punctuation).__name__}")
             # The above line raises type error, but it's an "an" instead of an "a"
 
     if case_insensitive:  # Check if case_insensitive is true or not
@@ -47,9 +47,9 @@ def is_palindrome(string, case_insensitive=False, whitespace_insensitive=False, 
 
     if whitespace_insensitive:  # Check if case_insensitive is true or not
         string = re.sub(pattern=r"\s", repl="", string=string)  # Uses regex to remove all whitespace characters, \s is
-        # a shorthand for [\t\n\r\f\v] with [] meaning any character in the brackets, \t = tabs, \n = newlines,
-        # \r = carriage returns, \f = form feeds, and \v = vertical tabs. re.sub substitutes any characters given in the
-        # first argument with the string in the second argument
+        # a shorthand for [ \t\n\r\f\v] with [] meaning any character in the brackets, " " = spaces \t = tabs,
+        # \n = newlines, \r = carriage returns, \f = form feeds, and \v = vertical tabs. re.sub substitutes any
+        # characters given in the first argument with the string in the second argument
 
     if ignore_punctuation:  # Check if case_insensitive is true or not
         string = re.sub(pattern=r"[^a-zA-Z0-9\s]", repl="", string=string)
